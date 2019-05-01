e0fe982d7e71d5e4f65b8ddfd7546643cd1b06a0
okbig <50184457+okbig@users.noreply.github.com>
2019-05-02 03:26:46 +0530

    Update 11_TelephoneBook.cpp

--- 11_TelephoneBook.cpp
+++ 11_TelephoneBook.cpp
@@ -1,6 +1,3 @@
-/*
-Consider telephone book database of N clients. Make use of a hash table implementation to quickly look up client‘s telephone number
-*/
 
 #include <iostream>
 #include <iomanip>
